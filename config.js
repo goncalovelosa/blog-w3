84f5516ffdb012400f0653eeaddf5eb7e399b494
Nino Velosa <ninovelosa@gmail.com>
2022-02-28 18:25:34 +0000

    Update

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 
-  export const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+  export const contractAddress = "0xC87b4c78FACED72AD166E14DB045346ed6B79a6E"
   export const ownerAddress = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
   
--- hardhat.config.js
+++ hardhat.config.js
@@ -6,13 +6,13 @@ module.exports = {
     hardhat: {
       chainId: 1337
     },
-    // mumbai: {
-    //   url: "https://rpc-mumbai.matic.today",
-    //   accounts: [process.env.pk]
-    // },
-    // polygon: {
-    //   url: "https://polygon-rpc.com/",
-    //   accounts: [process.env.pk]
-    // }
+    mumbai: {
+      url: "https://rpc-mumbai.matic.today",
+      accounts: [process.env.pk]
+    },
+    polygon: {
+      url: "https://polygon-rpc.com/",
+      accounts: [process.env.pk]
+    }
   }
 };
